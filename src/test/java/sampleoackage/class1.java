59ab3dbcb5729b25f3822f1ed9c55a4841582451
vijaykrishnapsr <vijaykrishna.psr@gmail.com>
2022-05-21 11:37:44 +0530

    Updated Codes

--- src/test/java/sampleoackage/class1.java
+++ src/test/java/sampleoackage/class1.java
@@ -1,5 +1,9 @@
 package sampleoackage;
 
 public class class1 {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 
 }
--- src/test/java/sampleoackage/sample2.java
+++ src/test/java/sampleoackage/sample2.java
@@ -1,5 +1,11 @@
 package sampleoackage;
 
 public class sample2 {
+	
+	public static void main(String[] args) {
+		System.out.println("Vijayakrishnamoorthy");
+	}
+	
+	
 
 }
